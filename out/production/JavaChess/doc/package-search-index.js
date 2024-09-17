a39dae4960f8ca961ae982afd159753c7c9e2c1d
Suleyman Farajli <suleyman@farajli.net>
2024-09-17 20:19:00 +0400

    out directory removed

--- out/production/JavaChess/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"chess"},{"l":"chess.gui"},{"l":"chess.logic.board"},{"l":"chess.logic.pieces"},{"l":"chess.logic.player"}];updateSearchResults();
